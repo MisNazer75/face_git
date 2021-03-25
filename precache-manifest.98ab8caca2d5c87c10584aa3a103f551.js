6a9a62b129ce36352f3807cae73b77d7e9982bcb
MisNazer75 <79902342+MisNazer75@users.noreply.github.com>
2021-03-25 17:42:28 +0430

    Updates

---
+++ precache-manifest.98ab8caca2d5c87c10584aa3a103f551.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eff597b421a76b5a43e",
+    "url": "/face_git/static/css/main.f5597596.chunk.css"
+  },
+  {
+    "revision": "8eff597b421a76b5a43e",
+    "url": "/face_git/static/js/main.8eff597b.chunk.js"
+  },
+  {
+    "revision": "abcb0016adc933c14021",
+    "url": "/face_git/static/js/1.abcb0016.chunk.js"
+  },
+  {
+    "revision": "98177cf29ded14510ea6",
+    "url": "/face_git/static/js/runtime~main.98177cf2.js"
+  },
+  {
+    "revision": "34e86f06102f347f0475d68f8673318b",
+    "url": "/face_git/static/media/test.34e86f06.jpg"
+  },
+  {
+    "revision": "cc46e63d574915a8430eed2a3f4fe165",
+    "url": "/face_git/static/media/example.cc46e63d.jpg"
+  },
+  {
+    "revision": "995f1d3c7e94e18448e6e3938aadd9e7",
+    "url": "/face_git/index.html"
+  }
+];
